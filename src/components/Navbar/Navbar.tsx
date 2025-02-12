fb9c464dcbe18ba1ddfa1ed86b2b4be2e54d55cd
PaaRich <siehrichmond50@gmail.com>
2025-02-12 02:02:37 +0000

    navbar

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,16 @@
+import { NavbarProps } from '../../../types'
+
+const Navbar = ({title,children}:NavbarProps) => {
+  return (
+      <div className='flex items-center justify-between h-[105px]'>
+          <h1 className='text-[var(--deep-grey)] font-bold text-4xl'>{title}</h1>
+
+          <div>
+              {children}
+          </div>
+          
+    </div>
+  )
+}
+
+export default Navbar
