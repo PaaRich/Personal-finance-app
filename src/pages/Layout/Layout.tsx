40b33d23ce9efa08255b94624ff8822f0bedce68
PaaRich <siehrichmond50@gmail.com>
2025-02-12 02:05:04 +0000

    layout

--- src/pages/Layout/Layout.tsx
+++ src/pages/Layout/Layout.tsx
@@ -1,17 +1,24 @@
 import styles from "./Layout.module.css";
 import { Outlet } from "react-router-dom";
 import SideBar from "../../components/SideBar/SideBar";
+import { useContext } from "react";
+import { Context } from "../../context/Context";
 
 const Layout = () => {
+  const {open,setOpen } = useContext(Context);
   return (
     <div className={styles.layout_container}>
-          {/* sidebar */}
-      <div>
-        <SideBar/>
-          </div>
+      {/* sidebar */}
+      <div className={`${open?"w-[320px]":"w-[76px]"} transition-all duration-300 ease-in-out`}>
+        <SideBar />
+      </div>
+        
+         
 
           {/* pages */}
-          <div><Outlet/></div>
+      <div className={` transition-all duration-300 ease-in-out w-full grow overflow-y-auto`}>
+        <div className="px-10"><Outlet /></div>
+      </div>
     </div>
   )
 }
