5e34d60d2ef2c93482b44abc870d39837dea4d46
PaaRich <siehrichmond50@gmail.com>
2025-02-12 01:56:25 +0000

    tailwind installation

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import type { UserConfigExport } from 'vitest/config';
+import tailwindcss from "@tailwindcss/vite"
 
 /// <reference types="vitest/config" />
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
   test: {
     globals:true,
     environment: 'jsdom', // Ensure this matches correctly
