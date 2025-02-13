65ccdcd1e54f3e38c6a3f5976a7eceb671e60c26
PaaRich <siehrichmond50@gmail.com>
2025-02-13 14:38:17 +0000

    styles

--- src/pages/Pots/SmallPotsCard.tsx
+++ src/pages/Pots/SmallPotsCard.tsx
@@ -2,7 +2,7 @@
 
 const SmallPotsCard = ({ color, label, amount }: { color: string; label: string; amount:string}) => {
   return (
-    <div className={`border-l-[5px] border-l-[${color}] pl-3`}>
+    <div style={{borderLeftColor:`${color}`}} className="border-l-[5px] pl-3">
           <p className="text-[var(--mid-grey)]">{ label}</p>
           <p className="font-bold text-[18px]">${ amount }</p>
     </div>
