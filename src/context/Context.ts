fdd865ddd8458179d28b97275740bf36ecae8728
PaaRich <siehrichmond50@gmail.com>
2025-02-13 14:48:37 +0000

    defined type for context

--- src/context/Context.ts
+++ src/context/Context.ts
@@ -1,3 +1,17 @@
 import { createContext } from "react";
+
+type p = {
+    name: string;
+    value: number;
+    color: string;
+}
+interface ContextProp{
+    open: boolean;
+    setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+    openPopUp: boolean;
+    setOpenPopUp: React.Dispatch<React.SetStateAction<boolean>>;
+    data: p[];
+
+}
 //how to define the context
-export const Context = createContext({});
+export const Context = createContext<ContextProp>({} as ContextProp);
