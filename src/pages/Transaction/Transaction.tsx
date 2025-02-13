9103ab247fb9f8155e6ceecb652c7cbd39bfc9c4
PaaRich <siehrichmond50@gmail.com>
2025-02-13 14:36:37 +0000

    sort,category portal control

--- src/pages/Transaction/Transaction.tsx
+++ src/pages/Transaction/Transaction.tsx
@@ -2,10 +2,16 @@ import Navbar from "../../components/Navbar/Navbar";
 import TransactionDetail from "./TransactionDetail";
 import profilePic from "../../assets/Person 1.jpg"
 import { AiOutlineSearch } from "react-icons/ai";
+import { IoMdArrowDropleft, IoMdArrowDropright } from "react-icons/io";
 import { useState } from "react";
+// import { useSelector } from "react-redux";
 
 const Transaction = () => {
   const [search, setSearch] = useState('');
+  const [sort, setSort] = useState<string>("latest")
+  const [category, setCategory] = useState<string>('all transactions');
+  // const transactions=useSelector((state)=>state.)
+
   return (
     <div>
       <Navbar title="Transaction"/>
@@ -19,8 +25,8 @@ const Transaction = () => {
                {/* sort */}
             <div>
               <label className="mr-3" htmlFor="">Sort by</label>
-              <select className="outline-0 border-2 p-3 rounded-[10px] text-[18px]" name="sort">
-                <option selected value="latest">latest</option>
+              <select className="outline-0 border-2 p-3 rounded-[10px] text-[18px]" name="sort" value={sort} onChange={(e)=>setSort(e.target.value)}>
+                <option value="latest">latest</option>
                 <option value="A to Z">A to Z</option>
                 <option value="Z to A">Z to A</option>
                 <option value="highest">highest</option>
@@ -31,8 +37,8 @@ const Transaction = () => {
             {/* filter */}
            <div>
               <label className="mr-3" htmlFor="">Category</label>
-              <select className="outline-0 border-2 p-3 rounded-[10px] text-[18px]" name="category">
-                <option selected value="all transactions">All transactions</option>
+              <select className="outline-0 border-2 p-3 rounded-[10px] text-[18px]" name="category" value={category} onChange={(e)=>setCategory(e.target.value)}>
+                <option value="all transactions">All transactions</option>
                 <option value="entertainment">Entertainment</option>
                 <option value="bills">Bills</option>
                 <option value="groceries">Groceries</option>
@@ -61,6 +67,19 @@ const Transaction = () => {
               <TransactionDetail img={ profilePic} name="Sie Richmond" category="Grocery" trancDate="17 Jun 2018" amount="40.09" />
             </tbody>
           </table>
+        </div>
+
+        {/* pagination */}
+        <div className="flex items-center justify-between mt-5">
+          <div className="py-2 px-3 border-2 bg-transparent rounded-[10px] flex items-center justify-around w-[94px]  hover:bg-[#93674f] hover:text-white duration-200 cursor-pointer " onClick={()=>{console.log("Prev")}}><IoMdArrowDropleft size={25}/>Prev</div>
+          <div>
+            <span className="rounded-[10px] cursor-pointer mr-3 py-2 px-3 text-[18px] hover:text-white hover:bg-[#93674f] duration-200 border-2 border-[#93674f]" onClick={()=>{console.log("hello")}}>1</span>
+            <span className="rounded-[10px] cursor-pointer mr-3 py-2 px-3 text-[18px] hover:text-white hover:bg-[#93674f] duration-200 border-2 border-[#93674f]" onClick={()=>{console.log("hello")}}>2</span>
+            <span className="rounded-[10px] cursor-pointer mr-3 py-2 px-3 text-[18px] hover:text-white hover:bg-[#93674f] duration-200 border-2 border-[#93674f]" onClick={()=>{console.log("hello")}}>3</span>
+            <span className="rounded-[10px] cursor-pointer mr-3 py-2 px-3 text-[18px] hover:text-white hover:bg-[#93674f] duration-200 border-2 border-[#93674f]" onClick={()=>{console.log("hello")}}>4</span>
+            <span className="rounded-[10px] cursor-pointer mr-3 py-2 px-3 text-[18px] hover:text-white hover:bg-[#93674f] duration-200 border-2 border-[#93674f]  " onClick={()=>{console.log("hello")}}>5</span>
+          </div>
+          <div className="py-2 px-3 border-2 bg-transparent rounded-[10px] flex items-center justify-around w-[94px] hover:bg-[#93674f] hover:text-white duration-200 cursor-pointer" onClick={()=>{console.log("next")}}>Next<IoMdArrowDropright size={25}/></div>
         </div>
       </div>
     </div>
