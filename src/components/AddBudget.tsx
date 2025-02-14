c115fdda1cc003949008c5f2c0a1641ad90c3c95
PaaRich <siehrichmond50@gmail.com>
2025-02-14 03:13:34 +0000

    add and delete budget

---
+++ src/components/AddBudget.tsx
@@ -0,0 +1,42 @@
+import PopUp from "./PopUp"
+import { ColorDropdown } from "../pages/Budget/ColorDropdown";
+import { useContext,useState } from "react";
+import { Context } from "../context/Context";
+
+
+const AddBudget = () => {
+    const { setOpenPopUp } = useContext(Context);
+      const [budgetAmount, setBudgetAmount] = useState<string>()
+  return (
+    <PopUp
+          label="Add New Budget"
+          description="Lorem ipsum dolor sit amet consectetur quod illo enim impedit dolores."
+          btnLabel="Add Budget"
+          submitFn={() => console.log("New Budget Created")}
+          closeFn={()=>setOpenPopUp(false)}
+        >
+          <div>
+            <label htmlFor="category">Budget Category</label>
+            <select className="w-full border-2 rounded-[10px] p-3 outline-0 cursor-pointer mt-2" name="category" id="">
+              <option value="entertainment">Entertainment</option>
+              <option value="bills">Bills</option>
+              <option value="groceries">Groceries</option>
+              <option value="dinning-out">Dinning Out</option>
+              <option value="transportation">Transportation</option>
+            </select>
+          </div>
+          
+          <div className="my-4">
+            <label htmlFor="">Maximum Spend</label>
+          <input className="w-full rounded-[10px] border-2 outline-0 p-3 mt-2" type="number" min="1" placeholder="e.g $200" value={budgetAmount} onChange={(e:React.ChangeEvent<HTMLInputElement>)=>{setBudgetAmount(e.target.value)}}/>
+          </div>
+          
+          <div>
+            <label htmlFor="category">Theme</label>
+            <ColorDropdown />
+          </div>
+        </PopUp>
+  )
+}
+
+export default AddBudget
---
+++ src/components/EditBudget.tsx
@@ -0,0 +1,40 @@
+import PopUp from "./PopUp"
+import { useContext } from "react";
+import { Context } from "../context/Context";
+import { ColorDropdown } from "../pages/Budget/ColorDropdown";
+
+const EditBudget = () => {
+  const { setOpenEditPopUp } = useContext(Context);
+  return (
+    <PopUp
+      description="You about to delete this budget"
+      label="Edit Budget"
+      btnLabel="Save Changes"
+      submitFn={() => { console.log("Saved Changes") }}
+      closeFn={()=>setOpenEditPopUp(false)}
+    >
+        <div>
+                    <label htmlFor="category">Budget Category</label>
+                    <select className="w-full border-2 rounded-[10px] p-3 outline-0 cursor-pointer mt-2" name="category" id="">
+                      <option value="entertainment">Entertainment</option>
+                      <option value="bills">Bills</option>
+                      <option value="groceries">Groceries</option>
+                      <option value="dinning-out">Dinning Out</option>
+                      <option value="transportation">Transportation</option>
+                    </select>
+                  </div>
+                  
+                  <div className="my-4">
+                    <label htmlFor="">Maximum Spend</label>
+                  <input className="w-full rounded-[10px] border-2 outline-0 p-3 mt-2" type="number" />
+                  </div>
+                  
+                  <div>
+                    <label htmlFor="category">Theme</label>
+                    <ColorDropdown />
+                  </div>
+    </PopUp>
+  )
+}
+
+export default EditBudget;
