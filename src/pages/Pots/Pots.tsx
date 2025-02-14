cfcfcfde36c96c8848e8c75822725f087b510116
PaaRich <siehrichmond50@gmail.com>
2025-02-14 03:22:02 +0000

    button onclick fn

--- src/pages/Pots/Pots.tsx
+++ src/pages/Pots/Pots.tsx
@@ -1,12 +1,16 @@
 import Navbar from "../../components/Navbar/Navbar";
 import Button from "../../components/Button/Button";
 import PotCard from "./PotCard";
+import { useContext } from "react";
+import { Context } from "../../context/Context";
 
 const Pots = () => {
+  const { setOpenPot} = useContext(Context);
+
   return (
     <div>
       <Navbar title="Pots">
-        <Button title="Add New Pot" onclick={()=>console.log("Add Pot")}/>
+        <Button title="Add New Pot" onclick={()=>{setOpenPot(true)}}/>
       </Navbar>
       <div className="grid grid-cols-[repeat(auto-fit,minmax(343px,1fr))] md:grid-cols-[repeat(auto-fit,minmax(689px,1fr))] lg:grid-cols-[repeat(auto-fit,minmax(518px,1fr))] gap-5">
         <PotCard color="#277c78" label="Savings" target={2000} amountSaved={159}/>
