170d7ee85ce03fc3531e612bea9a683f8a6e44f0
PaaRich <siehrichmond50@gmail.com>
2025-02-14 03:22:29 +0000

    redux store

---
+++ src/redux/store.ts
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit';
+import transactionReducer from "./Features/Transaction.ts"
+
+export const store = configureStore({
+  reducer: {
+    transactions:transactionReducer
+  },
+});
+
+// Infer RootState and AppDispatch types from the store
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
