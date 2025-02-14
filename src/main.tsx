cfe4c8592afc809ba5df581e2bbaa12b1bf81998
PaaRich <siehrichmond50@gmail.com>
2025-02-14 03:12:19 +0000

    redux store

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,18 @@ import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom';
 import { AppContext } from './context/AppContext.tsx';
+import { Provider } from 'react-redux';
+import {store} from "./redux/store.ts";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <AppContext>
+      <Provider store={store}>
+        <AppContext>
         <App />
       </AppContext>
+      </Provider>
+      
     </BrowserRouter>
     
   </StrictMode>,
