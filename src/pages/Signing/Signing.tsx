3716a84a052683f992a72d627c28ee764412ff37
PaaRich <siehrichmond50@gmail.com>
2025-03-13 08:00:08 +0000

    optimise page

--- src/pages/Signing/Signing.tsx
+++ src/pages/Signing/Signing.tsx
@@ -4,11 +4,11 @@ import { Outlet } from "react-router-dom";
 const signing = () => {
   return (
     <div className="h-dvh p-5 flex items-center bg-[#f2f3f7] relative">
-      <div className="hidden lg:block bg-black text-white py-8 rounded-2xl">
+      <div className="hidden lg:block bg-black text-white py-8 rounded-2xl h-full">
         <p className="pl-5 font-bold text-2xl">finance</p>
-        <img className="bg-black rounded-2xl w-[65%]" src={signInLogo} alt="logo" />
-        <div className="px-5 mt-5">
-          <p className="font-bold text-2xl mb-5">Keep track of your money <br />
+        <img className="bg-black rounded-2xl w-[60%]" src={signInLogo} alt="logo" />
+        <div className="pl-5 mt-5">
+          <p className="font-bold text-2xl mb-3">Keep track of your money <br />
             and save for your future.</p>
           <p>Personal finance app puts you in control of your spending.
             Track transactions,set budgets and add to savings pot easily.
