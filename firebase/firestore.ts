dcc1da41ef12879d8a7738dd9b67b5667ce13974
PaaRich <siehrichmond50@gmail.com>
2025-03-16 08:18:06 +0000

    firestore setup

--- firebase/firestore.ts
+++ firebase/firestore.ts
@@ -1,16 +1,38 @@
-import {
-    getFirestore
-} from "firebase/firestore";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
 import app from "./firebaseConfig";
+import { potItemProp } from "../types";
+import { ThunkAction } from "redux-thunk";
+import { AnyAction } from "redux";
+import { RootState } from "../src/redux/store";
+import { toast } from "react-toastify";
 
 const db = getFirestore(app);
 
-// const collectionRef = collection(db, "budgets");
+const budgetCollectionRef = collection(db, "Budgets");
+const potsCollectionRef = collection(db, 'Pots');
 
-// getDocs(collectionRef)
-//     .then(snapshot => {
-//         snapshot.forEach(doc => {
-//             console.log(doc.data());
-//             });
-//     })
-export default db;
+
+
+// add pot
+const addPotFunc = (item: potItemProp): ThunkAction<void, RootState, unknown, AnyAction> => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    return async (dispatch) => {
+        try {
+             await addDoc(potsCollectionRef, item);
+            toast.success("Pot added successfully");
+            console.log("items added", item);
+            
+        } catch (err:unknown) {
+            if (err instanceof Error) {
+                toast.error(err.message);
+                console.log(err.message);
+            } else {
+                toast.error("An unknown error occurred");
+                console.log("An unknown error occurred", err);
+            }
+            // Dispatch a failure action if needed
+        }
+    };
+};
+
+export { db, budgetCollectionRef, potsCollectionRef, addPotFunc };
