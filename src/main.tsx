e22b4324db89d9e31326191ee48e33c7ad615334
PaaRich <siehrichmond50@gmail.com>
2025-03-16 08:19:48 +0000

    store update

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom';
 import { AppContext } from './context/AppContext.tsx';
 import { Provider } from 'react-redux';
-import {store} from "./redux/store.ts";
+import { store } from "./redux/store.ts";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
