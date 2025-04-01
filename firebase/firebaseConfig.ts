5c529dfee408456b74940474920b2c32e6ecf8d7
PaaRich <siehrichmond50@gmail.com>
2025-04-01 03:44:54 +0000

    moved auth to here

--- firebase/firebaseConfig.ts
+++ firebase/firebaseConfig.ts
@@ -1,5 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import {
+    getAuth,
+} from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -12,5 +15,5 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-export default app;
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(); // Initialize Firebase Authentication and get a reference to the service
