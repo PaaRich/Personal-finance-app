1b7a9e3794116b8223598bb2cbeca2ecbfcb7d67
PaaRich <siehrichmond50@gmail.com>
2025-04-01 03:47:38 +0000

    added auth provider

--- src/main.tsx
+++ src/main.tsx
@@ -6,14 +6,17 @@ import { BrowserRouter } from 'react-router-dom';
 import { AppContext } from './context/AppContext.tsx';
 import { Provider } from 'react-redux';
 import { store } from "./redux/store.ts";
+import { AuthProvider } from './context/AuthProvider.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Provider store={store}>
+      <AuthProvider>
         <AppContext>
         <App />
-      </AppContext>
+          </AppContext>
+        </AuthProvider>
       </Provider>
       
     </BrowserRouter>
