4d5ec70997934b617225e7ec986529af4cea2bf6
PaaRich <siehrichmond50@gmail.com>
2025-05-07 00:38:40 +0000

    react-skeleton-loading install

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import { AppContext } from './context/AppContext.tsx';
 import { Provider } from 'react-redux';
 import { store } from "./redux/store.ts";
 import { AuthProvider } from './context/AuthProvider.tsx';
+import 'react-loading-skeleton/dist/skeleton.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
